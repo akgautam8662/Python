28aac07de9dd6277e2a39e5e71127ba6c3ecb04a
Ashish Kumar Gautam <163851633+akgautam8662@users.noreply.github.com>
2024-03-19 22:50:48 +0530

    Add files via upload

---
+++ 22_LAMBDA_FUN.py
@@ -0,0 +1,17 @@
+# LAMBDA FUNCTION : 
+
+# A lambda function is a small anonymous function.
+
+# A lambda function can take any number of arguments, but can only have one expression.
+
+def sum(a,b):
+    return a+b
+
+print(sum(5,7))
+
+
+sum=lambda a,b :a+b 
+print(sum(12,8))
+
+avg=lambda x,y:(x+y)/2
+print(avg(5,5))
