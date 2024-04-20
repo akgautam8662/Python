50f5743964687d43b8992ad7b74b22c1a2da470c
Ashish Kumar Gautam <163851633+akgautam8662@users.noreply.github.com>
2024-04-20 18:14:20 +0530

    Delete PANDAS LIBRARY directory

--- PANDAS LIBRARY/2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data={
-      "Name":["Ashish","Aman","Vivek","Juhi"],
-      "Age":[23,20,22,21],
-      "Salary": [45000,44000,45500,30000]
-      }
-df=pd.DataFrame(data)
-print(df)
